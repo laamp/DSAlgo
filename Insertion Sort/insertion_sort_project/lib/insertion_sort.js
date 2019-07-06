efb798d0db400308ed062d76a26b5526b4fab30c
Lance Smith <lance.matthew.smith@gmail.com>
2019-07-05 23:20:36 -0700

    Insertion sort

---
+++ Insertion Sort/insertion_sort_project/lib/insertion_sort.js
@@ -0,0 +1,18 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currElement = arr[i];
+    let j = 0;
+
+    for (j = i - 1; j >= 0 && currElement < arr[j]; j--) {
+      arr[j + 1] = arr[j];
+    }
+
+    arr[j + 1] = currElement;
+  }
+
+  return arr;
+}
+
+module.exports = {
+  insertionSort
+};
